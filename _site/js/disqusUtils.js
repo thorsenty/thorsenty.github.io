8b5448f35dc17097029e2dc7b82a5b2441223af2
Ty Thorsen <thorsenty@gmail.com>
2014-04-01 23:04:41 -0500

    add disqus utility js

---
+++ _site/js/disqusUtils.js
@@ -0,0 +1,27 @@
+Thorsent.disqusUtils = {
+
+	shortname: "thorsent",
+	apiKey: "cuWxmYM9HB2OlZanqsKfIa1R1kXIuLa9pv479c4KqfIIbwA2sSK5Ho1hIAHp6WiS",
+
+	getThreadComments: function(threadId) {
+		var comments = 0;
+		this.getThreadDetails(threadId).then(function(data) {
+			if (data.code === 0) {
+				comments = data.response.posts;
+			} else {
+				console.log(data.code);
+			}
+		}, function(data) {
+			console.log("Error in thread details service for", threadId);
+		});
+		return comments;
+	},
+
+	getThreadDetails: function(threadId) {
+		var endpoint = "http://disqus.com/api/3.0/threads/details.json?api_key=" + this.apiKey + "&forum=" + this.shortname + "&thread:ident=" + threadId;
+
+		return $.ajax({
+			url: endpoint
+		});
+	}
+}
