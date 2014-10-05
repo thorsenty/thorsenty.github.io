28eef73461c15ea907075fcdd44a0294ae946ea3
Ty Thorsen <thorsenty@gmail.com>
2014-10-05 15:09:38 -0500

    add sass and autoprefixer grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,61 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      build: {
+    
+    autoprefixer: {
+      all: {
+        expand: true,
+        flatten: true,
+        src: "styles/*.css",
+        dest: "styles/dist/"
+      }
+    },
+
+    sass: {
+      dist: {
         files: [{
           expand: true,
-          cwd: "js",
+          cwd: "styles",
+          src: "*.scss",
+          dest: "styles",
+          ext: ".css"
+        }],
+        options: {
+          style: "compressed"
+        }
+      }
+    },
+
+    shell: {
+      jekyllBuild: {
+        command: "jekyll build --drafts"
+      },
+      jekyllServe: {
+        command: "jekyll serve"
+      }
+    },
+    
+    uglify: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: "scripts",
           src: "*.js",
-          dest: "js/build",
+          dest: "scripts/dist",
           ext: ".min.js"
         }]
       }
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-shell');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('serve', ['shell:jekyllServe']);
+  grunt.registerTask('default', ['uglify', 'sass', 'autoprefixer', 'shell:jekyllBuild']);
 
 };
